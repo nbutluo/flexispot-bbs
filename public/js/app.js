13b357c7fd2bef99fb03edbd67e3735386218247
nbutluo <nbutluo@163.com>
2021-08-03 17:54:30 +0800

    修复无法编译css

--- public/js/app.js
+++ public/js/app.js
@@ -37339,8 +37339,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/vagrant/Code/larabbs/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/vagrant/Code/larabbs/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/Code/flexispot-bbs/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/Code/flexispot-bbs/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
