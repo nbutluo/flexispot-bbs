e80fe4dee9dcf5256ae63fa3c93eb625d5671cbe
nbutluo <nbutluo@163.com>
2021-08-12 10:59:01 +0800

    邮箱验证样式修改

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,28 +1,29 @@
 @extends('layouts.app')
-
+@include('shared._messages')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+<div class="email-box">
+  <p class="title">{{ __('Verify Your Email Address') }}</p>
 
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
-                        </div>
-                    @endif
-
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
-                    <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
-                        @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                    </form>
-                </div>
-            </div>
-        </div>
+  @if (session('resent'))
+  <div data-show="true" class="ant-alert alert-success verify-email-message" role="alert">
+    <span role="img" aria-label="check-circle" class="anticon anticon-check-circle ant-alert-icon">
+      <svg viewBox="64 64 896 896" focusable="false" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+        <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm193.5 301.7l-210.6 292a31.8 31.8 0 01-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.5 12.7z">
+        </path>
+      </svg>
+    </span>
+    <div class="ant-alert-content">
+      <div class="ant-alert-message"> {{ __('A fresh verification link has been sent to your email address.') }}
+      </div>
     </div>
+  </div>
+  @endif
+
+  <form class="d-inline" method="POST" action="{{ route('verification.resend') }}" name="verify-email">
+    <p class="info">{{ __('Before proceeding, please check your email for a verification link.') }}。{{ __('If you did not receive the email') }},
+      @csrf
+      <button type="submit">{{ __('Click here to request another') }}</button>
+    </p>
+  </form>
 </div>
 @endsection
