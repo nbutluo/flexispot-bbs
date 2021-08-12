a7f4aac55c9ec1e5d9ff50d6400e768152713d3b
nbutluo <nbutluo@163.com>
2021-08-12 11:02:27 +0800

    新建话题

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -1,66 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
+<form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8" name="topics-store">
+  @csrf
 
-<div class="container">
-  <div class="col-md-10 offset-md-1">
-    <div class="card ">
+  <div class="post-box">
+    @include('shared._error')
+    <input type="text" name="title" value="{{ old('title') }}" class="post-title" placeholder="Please fill in the title">
 
-      <div class="card-body">
-        <h2 class="">
-          <i class="far fa-edit"></i>
-          @if($topic->id)
-          编辑话题
-          @else
-          新建话题
-          @endif
-        </h2>
+    <select class="class" placeholder="Please select a classification" name="category_id" required>
+      <option value="">请选择分类</option>
+      @foreach ($categories as $value)
+      <option value="{{ $value->id }}">
+        {{ $value->name }}
+      </option>
+      @endforeach
+    </select>
 
-        <hr>
-
-        @if($topic->id)
-        <form action="{{ route('topics.update', $topic->id) }}" method="POST" accept-charset="UTF-8">
-          <input type="hidden" name="_method" value="PUT">
-          @else
-          <form action="{{ route('topics.store') }}" method="POST" accept-charset="UTF-8">
-            @endif
-
-            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-            @include('shared._error')
-
-            <div class="form-group">
-              <input class="form-control" type="text" name="title" value="{{ old('title', $topic->title ) }}" placeholder="请填写标题" required />
-            </div>
-
-            <div class="form-group">
-              <select class="form-control" name="category_id" required>
-                <option value="" hidden disabled {{ $topic->id ? '' : 'selected' }}>请选择分类</option>
-                @foreach ($categories as $value)
-                <option value="{{ $value->id }}" {{ $topic->category_id == $value->id ? 'selected' : '' }}>
-                  {{ $value->name }}
-                </option>
-                @endforeach
-              </select>
-            </div>
-
-            <div class="form-group">
-              <textarea name="body" class="form-control" id="editor" rows="6" placeholder="请填入至少三个字符的内容。" required>{{ old('body', $topic->body ) }}</textarea>
-            </div>
-
-            <div class="well well-sm">
-              <button type="submit" class="btn btn-primary"><i class="far fa-save mr-2" aria-hidden="true"></i> 保存</button>
-            </div>
-          </form>
-      </div>
+    <div>
+      <textarea name="body" id="editor"></textarea>
     </div>
+    <span class="save-btn" onclick="document.forms['topics-store'].submit()">PRESERVE</span>
   </div>
-</div>
-
+</form>
 @endsection
 
 @section('styles')
 <link rel="stylesheet" type="text/css" href="{{ asset('css/simditor.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/new.css') }}">
 @stop
 
 @section('scripts')
