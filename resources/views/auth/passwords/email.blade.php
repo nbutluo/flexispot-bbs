deda3f48e04215d3741bb112933b554aabb709e9
nbutluo <nbutluo@163.com>
2021-08-16 09:32:49 +0800

    忘记密码页

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -6,23 +6,51 @@
 
 @section('content')
 <div class="login-form">
-  <div class="login-part">
-    <div class="login-title">
-      <h1>Forgot Your Password</h1>
-    </div>
-    <div class="text-box">
-      <p class="info">
-        Please enter your email address below to receive a password reset link.
-      </p>
-    </div>
-    <div class="form-item">
-      <label for="" class="title">Email</label>
-      <input type="text">
-    </div>
-    <div class="form-row" style="justify-content: space-between">
-      <a href="{{ route('login') }}" class="link">Go back</a>
-      <span class="action-btn">Reset My Password</span>
-    </div>
-  </div>
+    <form method="POST" action="{{ route('password.email') }}" name="password-reset">
+        @csrf
+        <div class="login-part">
+
+            @if (session('status'))
+            <div data-show="true" class="ant-alert alert-success" role="alert">
+                <span role="img" aria-label="check-circle" class="anticon anticon-check-circle ant-alert-icon">
+                    <svg viewBox="64 64 896 896" focusable="false" width="1em" height="1em" fill="currentColor" aria-hidden="true">
+                        <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm193.5 301.7l-210.6 292a31.8 31.8 0 01-51.7 0L318.5 484.9c-3.8-5.3 0-12.7 6.5-12.7h46.9c10.2 0 19.9 4.9 25.9 13.3l71.2 98.8 157.2-218c6-8.3 15.6-13.3 25.9-13.3H699c6.5 0 10.3 7.4 6.5 12.7z">
+                        </path>
+                    </svg>
+                </span>
+                <div class="ant-alert-content">
+                    <div class="ant-alert-message"> {{ session('status') }}</div>
+                    <div class="ant-alert-description"></div>
+                </div>
+            </div>
+            @endif
+
+            <div class="login-title">
+                <h1>{{ _('Forgot Your Password') }}</h1>
+            </div>
+
+            <div class="text-box">
+                <p class="info">
+                    {{ _('Please enter your email address below to receive a password reset link.') }}
+                </p>
+            </div>
+
+            <div class="form-item">
+                <label for="" class="title">Email</label>
+                <input type="email" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+
+                @error('email')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+                @enderror
+            </div>
+
+            <div class="form-row" style="justify-content: space-between">
+                <a href="{{ route('login') }}" class="link">Go back</a>
+                <span class="action-btn" onclick="document.forms['password-reset'].submit()">{{ _('Reset My Password') }}</span>
+            </div>
+        </div>
+    </form>
 </div>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,65 +5,55 @@
 @endsection
 
 @section('content')
-<div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-8">
-      <div class="card">
-        <div class="card-header">{{ __('Reset Password') }}</div>
 
-        <div class="card-body">
-          <form method="POST" action="{{ route('password.update') }}">
-            @csrf
+<div class="login-form">
+    <form method="POST" action="{{ route('password.update') }}" name="password-update">
+        @csrf
+        <input type="hidden" name="token" value="{{ $token }}">
 
-            <input type="hidden" name="token" value="{{ $token }}">
+        <div class="login-part">
+            <div class="login-title">
+                <h1>Reset Password</h1>
+            </div>
 
-            <div class="form-group row">
-              <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+            <div class="text-box">
+                <p class="info">Personal Information</p>
+            </div>
 
-              <div class="col-md-6">
-                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
+            <div class="form-item">
+                <label for="" class="title">Email</label>
 
+                <input name="email" type="email" placeholder="Email" class="@error('email') is-invalid @enderror" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
                 @error('email')
                 <span class="invalid-feedback" role="alert">
-                  <strong>{{ $message }}</strong>
+                    <strong>{{ $message }}</strong>
                 </span>
                 @enderror
-              </div>
+
             </div>
 
-            <div class="form-group row">
-              <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+            <div class="form-item">
+                <label for="" class="title">Password</label>
 
-              <div class="col-md-6">
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
+                <input type="password" class="@error('password') is-invalid @enderror" required name="password" required autocomplete="new-password">
                 @error('password')
                 <span class="invalid-feedback" role="alert">
-                  <strong>{{ $message }}</strong>
+                    <strong>{{ $message }}</strong>
                 </span>
                 @enderror
-              </div>
             </div>
 
-            <div class="form-group row">
-              <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+            <div class="form-item">
+                <label for="" class="title">Confirm Password</label>
 
-              <div class="col-md-6">
-                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-              </div>
+                <input type="password" name="password_confirmation" required autocomplete="new-password">
             </div>
 
-            <div class="form-group row mb-0">
-              <div class="col-md-6 offset-md-4">
-                <button type="submit" class="btn btn-primary">
-                  {{ __('Reset Password') }}
-                </button>
-              </div>
+            <div class="form-row" style="justify-content: space-between">
+                <a href="http://usbbs.test/login" class="link">Go back</a>
+                <span class="action-btn" onclick="document.forms['password-update'].submit()">Reset Password</span>
             </div>
-          </form>
         </div>
-      </div>
-    </div>
-  </div>
+    </form>
 </div>
 @endsection
