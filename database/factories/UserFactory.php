4b6c87d5a11b19e26aed1187e9d42fc71d71d290
nbutluo <nbutluo@163.com>
2021-08-19 10:49:19 +0800

    修改用户头像填充

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -13,12 +13,12 @@ class UserFactory extends Factory
     public function definition()
     {
         $avatars = [
-            'https://cdn.learnku.com/uploads/images/201710/14/1/s5ehp11z6s.png',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/Lhd1SHqu86.png',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/LOnMrqbHJn.png',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/xAuDMxteQy.png',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png',
-            'https://cdn.learnku.com/uploads/images/201710/14/1/NDnzMutoxX.png',
+            asset('assets/avatar/1.png'),
+            asset('assets/avatar/2.png'),
+            asset('assets/avatar/3.png'),
+            asset('assets/avatar/4.png'),
+            asset('assets/avatar/5.png'),
+            asset('assets/avatar/6.png'),
         ];
 
         return [
