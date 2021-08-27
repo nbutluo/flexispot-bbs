eb4691d4ec723160acc8dfbe47bfc2771d0c9893
nbutluo <nbutluo@163.com>
2021-08-27 18:00:31 +0800

    重定向跳转修改

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -7,116 +7,116 @@
 
 @section('content')
 <div class="banner-box">
-    <img src="{{ asset('assets/banner.png') }}" alt="" class="banner">
+  <img src="{{ asset('assets/banner.png') }}" alt="" class="banner">
 </div>
 
 <div class="forum-content" id="pjax-container">
-    <div class="left-panel">
-        <div class="card">
-            @include('pages._categories')
+  <div class="left-panel">
+    <div class="card">
+      @include('pages._categories')
+    </div>
+    @if ($announcements->is_show)
+    <div class="card">
+      <p class="title">Community Announcements</p>
+      <a href="{{ $announcements->link }}" target="_blank">
+        <div class="content">
+          {{ $announcements->content }}
         </div>
-        @if ($announcements->is_show)
-        <div class="card">
-            <p class="title">Community Announcements</p>
-            <a href="{{ $announcements->link }}" target="_blank">
-                <div class="content">
-                    {{ $announcements->content }}
-                </div>
-            </a>
-        </div>
+      </a>
+    </div>
+    @endif
+
+    <div class="card">
+      <p class="title">Community Highlight</p>
+      <div class="content">
+        @if (count($advertises))
+
+        @foreach ($advertises as $advertise)
+        <a href="{{ $advertise->link }}" target="_blank"> <img src="{{ $advertise->cover }}" alt="" class="discuss"></a>
+        @endforeach
+        @else
+        <img src="https://picsum.photos/334/278" alt="" class="discuss">
+
         @endif
+      </div>
+    </div>
+  </div>
 
-        <div class="card">
-            <p class="title">Community Highlight</p>
-            <div class="content">
-                @if (count($advertises))
-
-                @foreach ($advertises as $advertise)
-                <a href="{{ $advertise->link }}" target="_blank"> <img src="{{ $advertise->cover }}" alt="" class="discuss"></a>
-                @endforeach
-                @else
-                <img src="https://picsum.photos/334/278" alt="" class="discuss">
-
-                @endif
-            </div>
-        </div>
+  <div class="right-panel">
+    <div class="tabs-box">
+      <!-- <span class="tab active_tab all_tab">All categories</span> -->
+      <a href="{{ Request::url() }}"><span class="tab {{ active_tab_class (if_query('tab',null))}}">Latest</span></a>
+      <a href="?tab=top"><span class="tab {{ active_tab_class (if_query('tab','top'))}}">Top</span></a>
     </div>
 
-    <div class="right-panel">
-        <div class="tabs-box">
-            <!-- <span class="tab active_tab all_tab">All categories</span> -->
-            <a href="{{ Request::url() }}"><span class="tab {{ active_tab_class (if_query('tab',null))}}">Latest</span></a>
-            <a href="?tab=top"><span class="tab {{ active_tab_class (if_query('tab','top'))}}">Top</span></a>
+    <div class="discuss-list">
+
+      @if (count($topics))
+
+      @foreach ($topics as $topic)
+      <div class="discuss-box">
+        <div class="info">
+          <div class="title"><a href="{{ route('topics.show',$topic->id) }}" target="_blank">{{ $topic->title }}</a></div>
+          <p>
+            <span class="date"> Created on {{ $topic->created_at->formatLocalized('%B %d') }}</span>
+            <span class="{{active_categories_class($topic->category->id)}}">
+              <a href="{{ route('categories.show',$topic->category_id) }}" target="_blank">{{ $topic->category->name }}</a>
+            </span>
+          </p>
         </div>
-
-        <div class="discuss-list">
-
-            @if (count($topics))
-
-            @foreach ($topics as $topic)
-            <div class="discuss-box">
-                <div class="info">
-                    <div class="title"><a href="{{ route('topics.show',$topic->id) }}" target="_blank">{{ $topic->title }}</a></div>
-                    <p>
-                        <span class="date"> Created on {{ $topic->created_at->formatLocalized('%B %d') }}</span>
-                        <span class="{{active_categories_class($topic->category->id)}}">
-                            <a href="{{ route('categories.show',$topic->category_id) }}" target="_blank">{{ $topic->category->name }}</a>
-                        </span>
-                    </p>
-                </div>
-                <div class="ava">
-                    <a href="{{ route('users.show',$topic->user_id) }}" target="_blank"></a>
-                    <img src="{{ asset($topic->user->avatar) }}" alt="{{ $topic->user->name }}" onmouseover="showFloat(this)" onmouseout="hideFloat(this)" onclick="location.href='/users/{{ $topic->user_id }}' ">
-                    <div class="float-window" style="display: none;">
-                        <div class="head-row">
-                            <img src="{{ asset($topic->user->avatar) }}" alt="{{ $topic->user->name }}">
-                            <span>
-                                <span>{{ $topic->user->name }}</span>
-                                {{--<span>Lv.6</span>--}}
-                            </span>
-                        </div>
-                        <p class="row">
-                            <span>Member since: </span><span>{{ $topic->user->name }}</span>
-                        </p>
-                        <p class="row">
-                            <span>Posts:</span>
-                            <span>{{ $topic->user->posts()}}</span>
-                        </p>
-                        {{--
-                        <p class="row">
-                            <span>Likes received: </span>
-                            <span>36</span>
-                        </p>
-                        <p class="last-row">This user's public profile is hidden.</p>
-                        --}}
-                    </div>
-                </div>
-                <div class="nums">
-                    <span class="num">{{ $topic->view_count }}</span><span class="num">{{ $topic->reply_count }}</span>
-                </div>
+        <div class="ava">
+          <a href="{{ route('users.show',$topic->user_id) }}" target="_blank"></a>
+          <img src="{{ asset($topic->user->avatar) }}" alt="{{ $topic->user->name }}" onmouseover="showFloat(this)" onmouseout="hideFloat(this)" onclick="location.href=`{{ route('users.show',$topic->user_id) }}` ">
+          <div class="float-window" style="display: none;">
+            <div class="head-row">
+              <img src="{{ asset($topic->user->avatar) }}" alt="{{ $topic->user->name }}">
+              <span>
+                <span>{{ $topic->user->name }}</span>
+                {{--<span>Lv.6</span>--}}
+              </span>
             </div>
-            @endforeach
-
-            <div class="pagi-box">
-                @if (if_query('tab', 'top'))
-                {!! $topics->appends(Request::except('page'))->links('pagination::page') !!}
-                @else
-                {!! $topics->links('pagination::page') !!}
-                @endif
-            </div>
-
-            @else
-            <p style="color: #e3342f;"> 暂无数据</p>
-            @endif
+            <p class="row">
+              <span>Member since: </span><span>{{ $topic->user->name }}</span>
+            </p>
+            <p class="row">
+              <span>Posts:</span>
+              <span>{{ $topic->user->posts()}}</span>
+            </p>
+            {{--
+                <p class="row">
+                    <span>Likes received: </span>
+                    <span>36</span>
+                </p>
+                <p class="last-row">This user's public profile is hidden.</p>
+             --}}
+          </div>
         </div>
-    </div>
+        <div class="nums">
+          <span class="num">{{ $topic->view_count }}</span><span class="num">{{ $topic->reply_count }}</span>
+        </div>
+      </div>
+      @endforeach
 
-    <div class="control-btns">
-        <img src="/assets/arrow.png" alt="" onClick="goTop()" />
-        <a href="/new.html">
-            <img src="/assets/plus.png" alt="" />
-        </a>
+      <div class="pagi-box">
+        @if (if_query('tab', 'top'))
+        {!! $topics->appends(Request::except('page'))->links('pagination::page') !!}
+        @else
+        {!! $topics->links('pagination::page') !!}
+        @endif
+      </div>
+
+      @else
+      <p style="color: #e3342f;"> 暂无数据</p>
+      @endif
     </div>
+  </div>
+
+  <div class="control-btns">
+    <img src="/assets/arrow.png" alt="" onClick="goTop()" />
+    <a href="/new.html">
+      <img src="/assets/plus.png" alt="" />
+    </a>
+  </div>
 </div>
 @endsection
 
@@ -128,37 +128,37 @@
 <script src="{{ asset('js/nprogress.js') }}"></script>
 
 <script>
-    (function($) {
-        var LearnKu = {
-            init: function() {
-                var self = this;
+  (function($) {
+    var LearnKu = {
+      init: function() {
+        var self = this;
 
-                // Pjax 页面准备就绪的事件
-                $(document).on('pjax:end', function() {
-                    // 每一次 Pjax 请求完成后执行
-                    NProgress.done();
-                    self.siteBootUp();
-                });
+        // Pjax 页面准备就绪的事件
+        $(document).on('pjax:end', function() {
+          // 每一次 Pjax 请求完成后执行
+          NProgress.done();
+          self.siteBootUp();
+        });
 
-                // 第一次正常页面加载完成后执行
-                self.siteBootUp();
-            },
+        // 第一次正常页面加载完成后执行
+        self.siteBootUp();
+      },
 
-            siteBootUp: function() {
-                var self = this;
-                $(document).pjax('a', '#pjax-container');
-                //定义pjax有效时间，超过这个时间会整页刷新
-                $.pjax.defaults.timeout = 1200;
-                $(document).on('pjax:start', function() {
-                    NProgress.start();
-                });
-            },
-        };
-        window.LearnKu = LearnKu;
-    })(jQuery);
+      siteBootUp: function() {
+        var self = this;
+        $(document).pjax('a', '#pjax-container');
+        //定义pjax有效时间，超过这个时间会整页刷新
+        $.pjax.defaults.timeout = 1200;
+        $(document).on('pjax:start', function() {
+          NProgress.start();
+        });
+      },
+    };
+    window.LearnKu = LearnKu;
+  })(jQuery);
 
-    $(document).ready(function() {
-        LearnKu.init();
-    });
+  $(document).ready(function() {
+    LearnKu.init();
+  });
 </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,38 @@
 <?php
 
-Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/flarum');
 
-// 用户身份验证相关的路由
-Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
-Route::post('login', 'Auth\LoginController@login');
-Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+Route::get('/flarum', 'PagesController@root')->name('root');
 
-// 用户注册相关路由
-Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-Route::post('register', 'Auth\RegisterController@register');
+Route::prefix('flarum')->group(function () {
+    Auth::routes();
 
-// 密码重置相关路由
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+    Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-// Email 认证相关路由
-Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
-Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
-Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+    Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+    Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
-Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+    Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
-Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+    Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
-Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+    Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
+    // 子话题回复
+    Route::post('subreplies', 'RepliesController@subrepliestore')->name('subreplies.store');
 
-Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
+    Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
 
-Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]);
-// 子话题回复
-Route::post('subreplies', 'RepliesController@subrepliestore')->name('subreplies.store');
+    Route::get('announcements', 'AnnouncementController@show')->name('announcements.show');
+});
 
-Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+// 话题点赞
+Route::get('topic/like/{topic}', 'TopicsController@toggleLike')->name('topic.togglelike');
+// 话题收藏
+Route::get('topic/collect/{topic}', 'TopicsController@toggleCollect')->name('topic.togglecollect');
+// 话题点赞
+Route::get('reply/like/{reply}', 'RepliesController@toggleLike')->name('reply.togglelike');
 
 Route::get('admin', 'Admin\AdminController@index')->name('admin.index');
-
-Route::get('announcements', 'AnnouncementController@show')->name('announcements.show');
 
 Route::prefix('admin')->group(function () {
     Route::name('admin.')->group(function () {
@@ -55,10 +48,3 @@ Route::prefix('admin')->group(function () {
         Route::delete('logout', 'Admin\AdminController@logout')->name('logout');
     });
 });
-
-// 话题点赞
-Route::get('topic/like/{topic}', 'TopicsController@toggleLike')->name('topic.togglelike');
-// 话题收藏
-Route::get('topic/collect/{topic}', 'TopicsController@toggleCollect')->name('topic.togglecollect');
-// 话题点赞
-Route::get('reply/like/{reply}', 'RepliesController@toggleLike')->name('reply.togglelike');
