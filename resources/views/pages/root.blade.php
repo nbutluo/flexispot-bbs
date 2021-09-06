c0749d44aab760f248cb01ca133ee3ef37dc4d7c
nbutluo <nbutluo@163.com>
2021-09-06 10:22:02 +0800

    首页样式小调整

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -7,109 +7,113 @@
 
 @section('content')
 <div class="banner-box">
-    <img src="{{ asset('assets/banner.png') }}" alt="" class="banner">
+  <img src="{{ asset('assets/banner.png') }}" alt="" class="banner">
 </div>
 
 <div class="forum-content" id="pjax-container">
-    <div class="left-panel">
-        <div class="card">
-            @include('pages._categories')
+  <div class="left-panel">
+    <div class="card">
+      @include('pages._categories')
+    </div>
+    @if ($announcements->is_show)
+    <div class="card">
+      <p class="title">Community Announcements</p>
+      <a href="{{ $announcements->link }}" target="_blank">
+        <div class="content">
+          {{ $announcements->content }}
         </div>
-        @if ($announcements->is_show)
-        <div class="card">
-            <p class="title">Community Announcements</p>
-            <a href="{{ $announcements->link }}" target="_blank">
-                <div class="content">
-                    {{ $announcements->content }}
-                </div>
-            </a>
-        </div>
+      </a>
+    </div>
+    @endif
+
+    <div class="card">
+      <p class="title">Community Highlight</p>
+      <div class="content">
+        @if (count($advertises))
+
+        @foreach ($advertises as $advertise)
+        <a href="{{ $advertise->link }}" target="_blank"> <img src="{{ $advertise->cover }}" alt="" class="discuss"></a>
+        @endforeach
+        @else
+        <img src="https://picsum.photos/334/278" alt="" class="discuss">
+
         @endif
+      </div>
+    </div>
+  </div>
 
-        <div class="card">
-            <p class="title">Community Highlight</p>
-            <div class="content">
-                @if (count($advertises))
-
-                @foreach ($advertises as $advertise)
-                <a href="{{ $advertise->link }}" target="_blank"> <img src="{{ $advertise->cover }}" alt="" class="discuss"></a>
-                @endforeach
-                @else
-                <img src="https://picsum.photos/334/278" alt="" class="discuss">
-
-                @endif
-            </div>
-        </div>
+  <div class="right-panel">
+    <div class="tabs-box">
+      <!-- <span class="tab active_tab all_tab">All categories</span> -->
+      <a href="{{ Request::url() }}"><span class="tab {{ active_tab_class (if_query('tab',null))}}">Latest</span></a>
+      <a href="?tab=top"><span class="tab {{ active_tab_class (if_query('tab','top'))}}">Top</span></a>
     </div>
 
-    <div class="right-panel">
-        <div class="tabs-box">
-            <!-- <span class="tab active_tab all_tab">All categories</span> -->
-            <a href="{{ Request::url() }}"><span class="tab {{ active_tab_class (if_query('tab',null))}}">Latest</span></a>
-            <a href="?tab=top"><span class="tab {{ active_tab_class (if_query('tab','top'))}}">Top</span></a>
+    <div class="discuss-list">
+
+      @if (count($topics))
+
+      @foreach ($topics as $topic)
+      <div class="discuss-box">
+        <div class="info">
+          <a href="{{ route('topics.show',$topic->id) }}" target="_blank">
+            <div class="title">{{ $topic->title }}</div>
+          </a>
+          <p>
+            <span class="date"> Created on {{ $topic->created_at->formatLocalized('%B %d') }}</span>
+            <span class="{{active_categories_class($topic->category->id)}}">
+              <a href="{{ route('categories.show',$topic->category_id) }}"
+                target="_blank">{{ $topic->category->name }}</a>
+            </span>
+          </p>
         </div>
-
-        <div class="discuss-list">
-
-            @if (count($topics))
-
-            @foreach ($topics as $topic)
-            <div class="discuss-box">
-                <div class="info">
-                    <div class="title"><a href="{{ route('topics.show',$topic->id) }}" target="_blank">{{ $topic->title }}</a></div>
-                    <p>
-                        <span class="date"> Created on {{ $topic->created_at->formatLocalized('%B %d') }}</span>
-                        <span class="{{active_categories_class($topic->category->id)}}">
-                            <a href="{{ route('categories.show',$topic->category_id) }}" target="_blank">{{ $topic->category->name }}</a>
-                        </span>
-                    </p>
-                </div>
-                <div class="ava">
-                    <a href="{{ route('users.show',$topic->user_id) }}" target="_blank"></a>
-                    <img src="{{ asset($topic->user->avatar) }}" alt="{{ $topic->user->name }}" onmouseover="showFloat(this)" onmouseout="hideFloat(this)" onclick="location.href=`{{ route('users.show',$topic->user_id) }}` ">
-                    <div class="float-window" style="display: none;">
-                        <div class="head-row">
-                            <img src="{{ asset($topic->user->avatar) }}" alt="{{ $topic->user->name }}">
-                            <span>
-                                <span>{{ $topic->user->name }}</span>
-                                {{--<span>Lv.6</span>--}}
-                            </span>
-                        </div>
-                        <p class="row">
-                            <span>Member since: </span><span>{{ $topic->user->name }}</span>
-                        </p>
-                        <p class="row">
-                            <span>Posts:</span>
-                            <span>{{ $topic->user->posts()}}</span>
-                        </p>
-                        {{--
+        <div class="ava">
+          <a href="{{ route('users.show',$topic->user_id) }}" target="_blank"></a>
+          <img src="{{ asset($topic->user->avatar) }}" alt="{{ $topic->user->name }}" onmouseover="showFloat(this)"
+            onmouseout="hideFloat(this)" onclick="location.href=`{{ route('users.show',$topic->user_id) }}` ">
+          <div class="float-window" style="display: none;">
+            <div class="head-row">
+              <img src="{{ asset($topic->user->avatar) }}" alt="{{ $topic->user->name }}">
+              <span>
+                <span>{{ $topic->user->name }}</span>
+                {{--<span>Lv.6</span>--}}
+              </span>
+            </div>
+            <p class="row">
+              <span>Member since: </span><span>{{ $topic->user->name }}</span>
+            </p>
+            <p class="row">
+              <span>Posts:</span>
+              <span>{{ $topic->user->posts()}}</span>
+            </p>
+            {{--
                 <p class="row">
                     <span>Likes received: </span>
                     <span>36</span>
                 </p>
                 <p class="last-row">This user's public profile is hidden.</p>
              --}}
-                    </div>
-                </div>
-                <div class="nums">
-                    <span class="num">{{ $topic->view_count }}</span><span class="num">{{ $topic->reply_count }}</span>
-                </div>
-            </div>
-            @endforeach
-
-            <div class="pagi-box">
-                @if (if_query('tab', 'top'))
-                {!! $topics->appends(Request::except('page'))->links('pagination::page') !!}
-                @else
-                {!! $topics->links('pagination::page') !!}
-                @endif
-            </div>
-
-            @else
-            <p style="color: #e3342f;"> {{ _('Oh, no data yet') }}</p>
-            @endif
+          </div>
         </div>
+        <div class="nums">
+          <span class="num">{{ $topic->view_count }}</span><span class="num">{{ $topic->reply_count }}</span>
+        </div>
+      </div>
+      @endforeach
+
+      <div class="pagi-box">
+        @if (if_query('tab', 'top'))
+        {!! $topics->appends(Request::except('page'))->links('pagination::page') !!}
+        @else
+        {!! $topics->links('pagination::page') !!}
+        @endif
+      </div>
+
+      @else
+      <p style="color: #e3342f;"> {{ _('Oh, no data yet') }}</p>
+      @endif
     </div>
+  </div>
 
 
 </div>
@@ -117,10 +121,10 @@
 
 @section('control-btns')
 <div class="control-btns">
-    <img src="{{ asset('assets/arrow.png') }}" alt="" onClick="goTop()" />
-    <a href="{{ route('topics.create') }}" target="_blank">
-        <img src="{{ asset('assets/plus.png') }}" alt="" />
-    </a>
+  <img src="{{ asset('assets/arrow.png') }}" alt="" onClick="goTop()" />
+  <a href="{{ route('topics.create') }}" target="_blank">
+    <img src="{{ asset('assets/plus.png') }}" alt="" />
+  </a>
 </div>
 @endsection
 
@@ -132,7 +136,7 @@
 <script src="{{ asset('js/nprogress.js') }}"></script>
 
 <script>
-    (function($) {
+  (function($) {
         var LearnKu = {
             init: function() {
                 var self = this;
