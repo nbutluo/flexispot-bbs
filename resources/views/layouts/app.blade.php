54b5fe56fee64760b57ab80ff082a9e15eeaf3fa
nbutluo <nbutluo@163.com>
2021-09-06 18:55:29 +0800

    修复头部 CSS 样式与话题创建重复

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,6 @@
   <link href="{{ mix('css/header.css') }}" rel="stylesheet">
   <link href="{{ mix('css/ant-message.css') }}" rel="stylesheet">
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-  <link href="{{ mix('css/index.css') }}" rel="stylesheet">
 
   @yield('styles')
 
