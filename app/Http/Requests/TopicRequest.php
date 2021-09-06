e58660129b9695eba92cd2d50f143b512590d8a2
nbutluo <nbutluo@163.com>
2021-09-06 10:57:53 +0800

    表单验证修改为英文

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -6,34 +6,36 @@ class TopicRequest extends Request
 {
     public function rules()
     {
-        switch($this->method())
-        {
+        switch ($this->method()) {
             // CREATE
             case 'POST':
             // UPDATE
             case 'PUT':
             case 'PATCH':
-            {
+
                 return [
-                    'title'       => 'required|min:2',
-                    'body'        => 'required|min:3',
+                    'title' => 'required|min:2',
+                    'body' => 'required|min:3',
                     'category_id' => 'required|numeric',
                 ];
-            }
+
             case 'GET':
             case 'DELETE':
             default:
-            {
+
                 return [];
-            };
         }
     }
 
     public function messages()
     {
         return [
-            'title.min' => '标题必须至少两个字符',
-            'body.min' => '文章内容必须至少三个字符',
+            'title.required' => 'The title can not be blank',
+            'category_id.required' => 'Please select the post category',
+            'body.required' => 'The content can not be blank',
+            'body.min' => 'The content cannot be less than three characters',
+            'title.min' => 'The title must be at least two characters',
+            'body.min' => 'The content of the post must not be less than three characters',
         ];
     }
 }
--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
 use Auth;
+use Illuminate\Foundation\Http\FormRequest;
 
 class UserRequest extends FormRequest
 {
@@ -15,8 +15,8 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
-            'email' => 'required|email|unique:users,email,' . Auth::id(),
+            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,'.Auth::id(),
+            'email' => 'required|email|unique:users,email,'.Auth::id(),
             'introduction' => 'max:80',
             // 'avatar' => 'mimes:jpeg,bmp,png,gif|dimensions:min_width=208,min_height=208',
             // 'avatar' => 'mimes:jpeg,bmp,png,gif',
@@ -26,12 +26,12 @@ class UserRequest extends FormRequest
     public function messages()
     {
         return [
-            'avatar.mimes' => '头像必须是 jpeg, bmp, png, gif 格式的图片',
-            'name.unique' => '用户名已被占用，请重新填写',
-            'name.regex' => '用户名只支持英文、数字、横杠和下划线。',
-            'name.between' => '用户名必须介于 3 - 25 个字符之间。',
-            'name.required' => '用户名不能为空。',
-            'email.unique' => '邮箱已被占用，请检查',
+            'avatar.mimes' => 'The avatar must be a picture in jpeg, bmp, png, gif format',
+            'name.unique' => 'Username has been taken, please fill in again',
+            'name.regex' => 'Usernames only support English, numbers, horizontal bars and underscores.',
+            'name.between' => 'Username must be between 3-25 characters.。',
+            'name.required' => 'Username can not be empty.',
+            'email.unique' => 'The mailbox is already occupied, please check',
         ];
     }
 }
