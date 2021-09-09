df365f09535655ff589ef946907f2474611aca28
nbutluo <nbutluo@163.com>
2021-09-09 14:24:16 +0800

    注册时随机生成头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Models\User;
+use Illuminate\Support\Arr;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -11,8 +12,17 @@ class UserObserver
 {
     public function saving(User $user)
     {
+        $avatars = [
+            asset('assets/avatar/1.png'),
+            asset('assets/avatar/2.png'),
+            asset('assets/avatar/3.png'),
+            asset('assets/avatar/4.png'),
+            asset('assets/avatar/5.png'),
+            asset('assets/avatar/6.png'),
+        ];
+
         if (empty($user->avatar)) {
-            $user->avatar = '/assets/avatar.png';
+            $user->avatar = asset(Arr::random($avatars));
         }
     }
 }
